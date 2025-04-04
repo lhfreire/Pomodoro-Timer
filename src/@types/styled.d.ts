c305003db191dc9d42488663274f7c1bd57d1f5f
Luis Freire <luis.henrique.freire@gmail.com>
2025-04-04 16:18:20 -0300

    cores e fontes

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -5,5 +5,5 @@ import { defaultTheme } from '../styles/themes/default';
 type ThemeType = typeof defaultTheme;
 
 declare module 'styled-componentes' {
-    
+    export interface DefaultTheme extends ThemeType {}
 }
