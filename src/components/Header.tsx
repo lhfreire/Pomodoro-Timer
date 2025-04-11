1a040330748bdf986a3325b5b827e1a551ce53a2
Luis Freire <luis.henrique.freire@gmail.com>
2025-04-11 16:41:06 -0300

    header and layout

--- src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-export function Header() {
-    return (
-        <div>
-            <Header/>
-            <h1>Home</h1>    
-        </div>
-    ) 
-}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import { HeaderContainer } from "./styles";
+
+import logo from '../../assets/Logo.svg';
+import { Scroll, Timer } from "phosphor-react";
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <img src={logo} alt="" />
+            <nav>
+                <a href="">
+                    <Timer size={24}/>
+                </a>
+                <a href="">
+                    <Scroll size={24}/>
+                </a>
+            </nav>
+        </HeaderContainer>
+    )
+
+    
+}
