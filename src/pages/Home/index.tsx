8cd71e2abe9e91528683e8c4c50b4a5997f12952
Luis Freire <luis.henrique.freire@gmail.com>
2025-04-24 16:22:40 -0300

    Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,37 @@
+import { Play } from "phosphor-react";
+import { CountdownContainer, FormContainer, HomeContainer, Saparator } from "./styles";
+
+export function Home() {
+    return (
+        <HomeContainer>
+           <form action="">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <input id="task" />
+
+                    <label htmlFor="">Durante</label>
+                    <input type="number" id="minutesAmount" />
+
+                    <span>minutos.</span>
+                </FormContainer>
+            
+
+
+                <CountdownContainer>
+                        <span>0</span>
+                        <span>0</span>
+                        <Saparator>:</Saparator>
+                        <span>0</span>
+                        <span>0</span>
+                </CountdownContainer>
+
+
+                <button type="submit">
+                    <Play size={24}/>
+                    Começar
+                </button>
+           </form>
+
+        </HomeContainer>
+    )
+}
